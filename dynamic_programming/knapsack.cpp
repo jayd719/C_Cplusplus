5673592dbf6dc8af1f9494c7726a05336013bc23
JD-MBP <jashansingh29@gmail.com>
2024-10-27 16:22:43 +0530

    knapsack top down

--- dynamic_programming/knapsack.cpp
+++ dynamic_programming/knapsack.cpp
@@ -9,6 +9,41 @@ int m = 10;
 
 int dp[5][11];
 
+int knapsack_top_down(int weight[], int values[], int knapsack_weight, int n)
+{
+    int table[n + 1][knapsack_weight + 1];
+
+    int i, j;
+
+    // intinitailze the table for base case
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= knapsack_weight; j++)
+        {
+            if (i == j)
+            {
+                table[i][j] = 0;
+            }
+        }
+    }
+
+    //
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= knapsack_weight; j++)
+        {
+            if (weight[i - 1] <= j)
+            {
+                table[i][j] = max(weight[n - 1] + table[i - 1][j - weight[i - 1]], table[i - 1][j]);
+            }else{
+                table[i][j] = table[i+1][j];
+            }
+        }
+    }
+
+    return table[n+1][knapsack_weight+1];
+}
+
 int knapsack_mem(int weight[], int values[], int knapsack_weight, int n)
 {
 
@@ -61,6 +96,7 @@ int main()
     printf("Using Simple Recursion %d\n", knapsack_rec(w, v, m, n));
     memset(dp, -1, sizeof(dp));
     printf("Using Memoiztion %d\n", knapsack_mem(w, v, m, n));
+    printf("Using Top Down %d\n", knapsack_top_down(w, v, m, n));
 
     return 0;
 }
